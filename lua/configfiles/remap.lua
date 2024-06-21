3493aadbbb02c9709f1423e52d2595ae4f9cc29c
czaun <boldizsar.czaun@gmail.com>
2024-06-21 14:18:39 +0200

    autoscroll and minor LSP function upgrade

--- lua/configfiles/remap.lua
+++ lua/configfiles/remap.lua
@@ -7,6 +7,10 @@ vim.keymap.set("n", "<leader>q", vim.cmd.Ex) -- <spc>vx = vim exit (to "menu")
 
 vim.keymap.set("n", "<leader>c", ":") -- command mode
 
+-- auto scroll
+vim.keymap.set('n', 'j', 'jzz')
+vim.keymap.set('n', 'k', 'kzz')
+
 -- ================== --
 -- + Plugin keymaps + --
 -- ================== --
@@ -17,3 +21,24 @@ vim.keymap.set('t', '<esc>', '<C-\\><C-n>')
 vim.keymap.set('t', '<C-t><C-t>', '<C-\\><C-n>:ToggleTerm<CR>')
 vim.keymap.set('n', '<C-n><C-n>', '<C-w><C-w>')
 vim.keymap.set('t', '<C-n><C-n>', '<C-\\><C-n><C-w><C-w>')
+
+-- + LSP
+vim.keymap.set('n', '<C-d>', 'gd') -- go to definition
+vim.keymap.set('n', '<C-l>', ':noh') -- clear search highlight
+
+-- ================== --
+-- + Keyboard remap + --
+-- ================== --
+vim.keymap.set('i', '<M-d>', '\\')
+vim.keymap.set('i', '<M-h>', '(')
+vim.keymap.set('i', '<M-t>', '{')
+vim.keymap.set('i', '<M-n>', '}')
+vim.keymap.set('i', '<M-s>', ')')
+
+vim.keymap.set('i', '<M-f>', '`')
+vim.keymap.set('i', '<M-g>', '-')
+vim.keymap.set('i', '<M-c>', '=')
+vim.keymap.set('i', '<M-r>', '+')
+
+vim.keymap.set('i', '<M-w>', '[')
+vim.keymap.set('i', '<M-v>', ']')
